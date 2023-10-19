d831e88573fdddad36104870e2f3e178e049a13f
farfromrefuge <dev@akylas.fr>
2023-10-19 21:33:26 +0200

    fix: dont bundle json in build so that user can filter json with rollup/webpack...

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,10 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-
+import alias from '@rollup/plugin-alias';
 import pkg from './package.json';
 
+const external = ['../../src/templates/abbreviations.json', '../../src/templates/aliases.json', '../../src/templates/country-names.json', '../../src/templates/country-to-lang.json', '../../src/templates/county-codes.json', '../../src/templates/state-codes.json', '../../src/templates/templates.json'];
 export default [
   {
     input: 'src/index.js',
@@ -16,14 +17,22 @@ export default [
       sourcemap: true,
       exports: 'default'
     },
+    external,
     plugins: [
-      json(),
+      json({
+        compact:true
+      }),
       resolve(),
       commonjs(),
       babel({
         exclude: ['node_modules/**']
       }),
       terser(),
+      alias({
+        entries: [
+          { find:/^.\/templates\/(.*)\.json/, replacement: '../../src/templates/$1.json' }
+        ]
+      }),
     ],
   },
   {
@@ -32,13 +41,21 @@ export default [
       { file: pkg.main, format: 'cjs', exports: 'default' },
       { file: pkg.module, format: 'es', exports: 'default' },
     ],
+    external,
     plugins: [
-      json(),
+      json({
+        compact:true
+      }),
       resolve(),
       commonjs(),
       babel({
         exclude: ['node_modules/**']
       }),
+      alias({
+        entries: [
+          { find:/^.\/templates\/(.*)\.json/, replacement: '../../src/templates/$1.json' }
+        ]
+      })
     ],
   },
 ];
